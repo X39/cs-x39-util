459fe20b5d1f51268a2c5500082bc2cecd568703
Marco Silipo <marco@silipo.de>
2022-04-21 01:38:25 +0200

    addition of (Async)Disposable

---
+++ X39.Util/AsyncDisposable.cs
@@ -0,0 +1,14 @@
+﻿namespace X39.Util;
+
+#if NET5_0_OR_GREATER
+public sealed class AsyncDisposable : IAsyncDisposable
+{
+    private readonly Func<ValueTask> _onDispose;
+    public AsyncDisposable() : this(() => ValueTask.CompletedTask) { }
+    public AsyncDisposable(Func<ValueTask> onDispose) { _onDispose = onDispose; }
+    public ValueTask DisposeAsync()
+    {
+        return _onDispose();
+    }
+}
+#endif
---
+++ X39.Util/Disposable.cs
@@ -0,0 +1,13 @@
+﻿namespace X39.Util;
+
+public sealed class Disposable : IDisposable
+{
+    private readonly Action _onDispose;
+    public Disposable() : this(() => { }) { }
+    public Disposable(Action onDispose) { _onDispose = onDispose; }
+    public Disposable([InstantHandle]Action onEnter, Action onDispose) : this(onDispose) { onEnter(); }
+    public void Dispose()
+    {
+        _onDispose();
+    }
+}
