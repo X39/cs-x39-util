ee22837ee98082058de48d39b6f73ff644e6f347
X39 <marco@silipo.de>
2022-05-17 23:20:10 +0200

    formatting

--- X39.Util/StringExtensionMethods.cs
+++ X39.Util/StringExtensionMethods.cs
@@ -28,25 +28,38 @@ public static class StringExtensionMethods
     [StringFormatMethod("format")]
     public static string Format(this string format, object arg0)
         => string.Format(format, arg0);
-    
+
     /// <inheritdoc cref="string.Format(string,object,object)"/>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [StringFormatMethod("format")]
     public static string Format(this string format, object arg0, object arg1)
         => string.Format(format, arg0, arg1);
-    
+
     /// <inheritdoc cref="string.Format(string,object,object, object)"/>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [StringFormatMethod("format")]
     public static string Format(this string format, object arg0, object arg1, object arg2)
         => string.Format(format, arg0, arg1, arg2);
-    
+
     /// <inheritdoc cref="string.Format(string,object[])"/>
     [Pure]
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     [StringFormatMethod("format")]
     public static string Format(this string format, params object[] args)
         => string.Format(format, args);
+
+    /// <inheritdoc cref="ToUri(string, UriKind)"/>
+    public static Uri ToUri(this string source)
+        => new(source);
+    
+    /// <summary>
+    /// Creates a new <see cref="Uri"/> from <paramref name="source"/>.
+    /// </summary>
+    /// <param name="source">The string containing a uri.</param>
+    /// <param name="uriKind">The kind of uri that <paramref name="source"/> is.</param>
+    /// <returns>A new <see cref="Uri"/> instance.</returns>
+    public static Uri ToUri(this string source, UriKind uriKind)
+        => new(source, uriKind);
 }
