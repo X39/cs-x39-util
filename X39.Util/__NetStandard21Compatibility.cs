b0a41185340090c216f67d0864da5181d2864849
Marco Silipo <marco@silipo.de>
2022-08-03 01:13:46 +0200

    adding missing netstandard2.1 compatibility data

---
+++ X39.Util/__NetStandard21Compatibility.cs
@@ -0,0 +1,52 @@
+﻿#if NETSTANDARD2_1
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable RedundantNameQualifier
+// ReSharper disable UnusedType.Global
+#pragma warning disable CA1050
+#pragma warning disable CS1591
+
+
+/// <summary>
+/// Specifies that when a method returns <see cref="ReturnValue"/>,
+/// the parameter may be null even if the corresponding type disallows it.
+/// </summary>
+[AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+public sealed class MaybeNullWhenAttribute : Attribute
+{
+    /// <summary>Initializes the attribute with the specified return value condition.</summary>
+    /// <param name="returnValue">
+    /// The return value condition. If the method returns this value, the associated parameter may be null.
+    /// </param>
+    public MaybeNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
+
+    /// <summary>Gets the return value condition.</summary>
+    public bool ReturnValue { get; }
+}
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
+public sealed class MaybeNullAttribute : Attribute
+{
+}
+public sealed class NotNullWhenAttribute : Attribute
+{
+    /// <summary>Initializes the attribute with the specified return value condition.</summary>
+    /// <param name="returnValue">
+    /// The return value condition. If the method returns this value, the associated parameter will not be null.
+    /// </param>
+    public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
+
+    /// <summary>Gets the return value condition.</summary>
+    public bool ReturnValue { get; }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Offers compatibility with dotnet core init.
+    /// </summary>
+    [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public static class IsExternalInit
+    {
+    }
+}
+#endif
